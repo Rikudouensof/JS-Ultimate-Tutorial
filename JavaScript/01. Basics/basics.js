5350a70fa47a603230bec2819648d960b589718c
Rikudou En Sof <rikudouensof@outlook.com>
2021-05-14 09:51:21 +0100

    first commit

---
+++ JavaScript/01. Basics/basics.js
@@ -0,0 +1,34 @@
+//console.log("hello world")
+
+
+//Variables
+let carName;
+var wehere = "Jagua";
+carName = "Porche";
+console.log(carName + " " + wehere);
+
+
+//Constant
+
+const car = {type:"Porche", model:"102 Emergency", color:"white"}
+console.log(car.color)
+
+
+//Operators
+
+
+//Arithemetic
+var firstInt = 5;
+var secondInt = 2;
+var addition_result = firstInt + secondInt;
+var subtraction_result = firstInt - secondInt;
+var divison_result = firstInt / secondInt;
+var multiplication_result = firstInt * secondInt;
+var ExponetNumber = firstInt** secondInt;
+var modNumbers = firstInt % secondInt;
+var incrementNumber = firstInt++;
+var decrementNumber = firstInt--;
+
+//Assignment
+
+
